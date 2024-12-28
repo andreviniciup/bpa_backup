8da7b271d1fa592d9165d1f124a57033fef0d04b
Arthur Mello <art.mello@outlook.com>
2024-12-28 12:12:44 -0300

    Implementação inicial do sistema de login e configuração do banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-from app import routes 
+# Configuração da URI de conexão com o banco de dados PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://desenv:desenv123@198.58.104.92:5432/testeBPA'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Define a chave secreta para a aplicação Flask
+app.secret_key = '12321444545453657455442345fdgdcbvxcxdvvbxbf345254546'  # Substituir por uma chave mais segura em produção
+
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import request
-from app import app
+from flask import request, render_template, redirect, url_for, session
+from app import app, db
+from app.models import User
 from app.utils.bpa_controller import BPAController
 from app.utils.bpa_view import BPAView
 
@@ -7,9 +8,37 @@ from app.utils.bpa_view import BPAView
 bpa_controller = BPAController()
 bpa_view = BPAView()
 
-# Rota que apenas coordena o fluxo entre controller e view
+# Rota que redireciona para a página de login
 @app.route("/", methods=["GET", "POST"])
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login = request.form['username']
+        senha = request.form['password']
+        # Verifica as credenciais no banco de dados
+        user = User.query.filter_by(login=login, senha=senha).first()
+        if user:
+            session['username'] = login
+            session['nom_usuario'] = user.nom_usuario  # Armazena o nome do usuário na sessão
+            return redirect(url_for('formulario_bpa'))
+        else:
+            return render_template('login.html', error='Credenciais inválidas')
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('username', None)
+    session.pop('nom_usuario', None)  # Remove o nome do usuário da sessão
+    return redirect(url_for('login'))
+
+# Rota que apenas coordena o fluxo entre controller e view
+@app.route("/formulario_bpa", methods=["GET", "POST"])
 def formulario_bpa():
+    if 'username' not in session:
+        return redirect(url_for('login'))
     if request.method == "POST":
         return bpa_controller.process_form(request.form)
     return bpa_view.render_form()
